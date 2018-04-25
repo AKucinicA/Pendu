fa90d3425d1f967004bb3dbc23a8bd0ec2ee6651
AKucinicA <35217885+AKucinicA@users.noreply.github.com>
2018-04-25 13:23:16 +0200

    Update pendu.js

--- pendu/pendu.js
+++ pendu/pendu.js
@@ -1,7 +1,31 @@
-
 // cette fonction teste si l'utilisateur a
 // bien saisie une et une seule lettre dans 
 // le champ de saisie
-function tester() {
-
+function testChar() {
+	let sasie = getElementById("main").value;
+	if (typeof saisie == "string"){
+		isInWord();
+	} else {
+		alert("Vous n'avez pas saisi une lettre.")
+	}
 }
+
+//cette fonction renvoie True ou False si la lettre est dans le mot ou non
+function isInWord(){}
+
+//cette fonction ajoute la lettre
+//dans le mot si isInWord() est true
+function inWord(){}
+
+//cette fonction réactualise le pendu si isInWord() est false
+function notInWord(){}
+
+//cette fonction félicite le joueur
+//et le renvoie à la page d'accueil 
+//si le mot est trouvé
+function motPlein(){}
+
+//cette fonction affiche le mot
+//et renvoie le joueur à la page
+//d'accueil si le pendu est fini
+function dessinFin(){}
