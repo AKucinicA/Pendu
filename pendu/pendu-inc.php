ed3bbf86d12c1a96eedda4a57d7c5aaadbf69878
AKucinicA <35217885+AKucinicA@users.noreply.github.com>
2018-04-25 13:36:50 +0200

    Update pendu-inc.php

--- pendu/pendu-inc.php
+++ pendu/pendu-inc.php
@@ -1,9 +1,49 @@
 <?php
-function
-$i=0;
-$MOTS = [];
-foreach(file("dict.data") as $line){
+	session_start();
+
+	$_SESSION['lettresJouees'] = array();
+	$_SESSION['nbTentatives'] = 0;
+	$_SESSION['motADeviner'] = array();
+
+	//Enregistre le mot à deviner dans une variable session
+	$_SESSION['motADeviner'] = $_POST['motADeviner'];
+
+	//Sauvegarder la longueur du mot
+	$longueurMot = strlen($_SESSION['motADeviner']);
+
+	function
+	$i=0;
+	$MOTS = [];
+	foreach(file("dict.data") as $line){
 		$tokens = explode("",$line);
 		$MOT["$line"] = $tokens;
 		$i=$i+1;
+
+	//Le jeu
+	echo "<html>
+		<head>
+  			<title>Pendu</title>
+  			<meta http-equiv="Content-Type" content="text/html; charset=utf8">
+  			<link rel="stylesheet" type="text/css" href="pendu.css">
+  			<script language="javascript" src="pendu.js"></script>
+		</head>
+		<body>
+
+		<h1> Pendu </h1>
+
+		<div id="main">
+    		<img id="pendu" src="images/4.png" />
+    		<table>
+        		<tr>
+            		<!-- Un exemple avec le mot "BICYCLETTE" -->
+            		<td>B</td><td></td><td>C</td><td></td><td>C</td><td></td><td>E</td><td></td><td></td><td>E</td>
+        		</tr>
+    		/table>
+    		<form action="" method="get" onsubmit="return tester();">
+        		<input type="text" name="lettre" />
+        		<input type="submit" value="Nouvelle lettre" />
+    		</form>
+		</div>
+		</body>
+		</html>"
 ?>
